070a0b2151bad6c2aa58b5212d573e54ee335194
Victor Axelsson <victor_axelsson@hotmail.com>
2015-12-30 10:22:37 +0100

    Removed some trash files

--- maintanance/seed.js
+++ maintanance/seed.js
@@ -4,7 +4,7 @@ var users = [{
     username: "krappen@krappen.se",
     password: "0a1e1f7e2a04309b170967d427fd240c903ba697", //cu3ik0t2x3g
     salt: "32986f5785be1e59bf9c5309d56a8eafcb94a533",
-    profile_pic: "https://scontent-arn2-1.xx.fbcdn.net/hprofile-xta1/v/t1.0-1/p160x160/11221953_853647771368833_2830742537220595183_n.jpg?oh=a7aebe8f3d0e822a080b388970181fc5&oe=56BF4B18",
+    profile_pic: "http://s3.favim.com/orig/45/baby-cute-seal-seal-pup-smile-Favim.com-400983.jpg",
     roles: ["review"],
 }];
 
